03868310dfcd06b86776ef17577dff30736935fd
Artem Tkachuk <arti17900@gmail.com>
2019-12-14 15:17:34 +0600

    Добавлена модель Like.

--- gallery/webapp/admin.py
+++ gallery/webapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from webapp.models import Photo, Comment
+from webapp.models import Photo, Comment, Like
 
 admin.site.register(Photo)
 admin.site.register(Comment)
+admin.site.register(Like)
--- gallery/webapp/models.py
+++ gallery/webapp/models.py
@@ -7,9 +7,18 @@ class Photo(models.Model):
     note = models.CharField(max_length=100, verbose_name='Подпись')
     create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     author = models.ForeignKey(User, related_name='photos', on_delete=models.CASCADE, verbose_name='Автор')
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
 
     def __str__(self):
         return self.note
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, related_name='user_likes', on_delete=models.CASCADE, verbose_name='Автор')
+    photo = models.ForeignKey('webapp.Photo', related_name='photo_likes', on_delete=models.CASCADE, verbose_name='Фото')
+
+    class Meta:
+        unique_together = ('author', 'photo')
 
 
 class Comment(models.Model):
